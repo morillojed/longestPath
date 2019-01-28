e9c8874923171d63078d09ce0b1f8386ad5b5372
jed.steven.e.morillo <jed.steven.e.morillo@accenture.com>
2019-01-28 18:23:23 +0800

    Final.

--- src/Main.java
+++ src/Main.java
@@ -11,14 +11,13 @@ import java.util.Set;
 
 public class Main {
 
-    static HashMap<Integer, ArrayList<Integer>> path = new HashMap<>();
-    static ArrayList<Integer> path2;
-    static int ctr=0;
-    static int[][] a;
-    static int maximumValue = 0;
+    private static HashMap<Integer, ArrayList<Integer>> path = new HashMap<>();
+    private static ArrayList<Integer> path2;
+    private static int ctr=0;
+    private static int[][] a;
 
     public static void main(String[] args) throws FileNotFoundException, IOException{
-        final File file = new File("/Users/jedstevenmorillo/Downloads/skirsesort.kitzbuehel/4x4.txt");
+        final File file = new File("/Users/jedstevenmorillo/Downloads/skirsesort.kitzbuehel/map.txt");
         final BufferedReader br = new BufferedReader(new FileReader(file));
         int j = 0;
         String st = br.readLine();
@@ -26,25 +25,36 @@ public class Main {
         while ((st = br.readLine()) != null) {
             addAsArray(st, j++, false);
         }
-        /*for (j=0; j<a.length; j++) {
-            for (int i=0; i<a[j].length; i++) {
-                System.out.print(a[j][i] + " ");
-            }
-            System.out.println();
-        }*/
 
-        System.out.println("Max Length of Calculated Path: " + String.valueOf(longestPath(a, 0,0,0)));
+        final int maximumPath = longestPath(a,0);
+        System.out.println("Length of calculated path: " + String.valueOf(maximumPath));
 
         Set set = path.entrySet();
         Iterator iterator = set.iterator();
+        int index = 0;
+        int maxDrop = 0;
         while (iterator.hasNext()) {
             Map.Entry entry = (Map.Entry)iterator.next();
-            // for (Integer value : entry.getValue())
-            System.out.println(entry);
+            ArrayList<Integer> maxPath = (ArrayList)entry.getValue();
+            if (maxPath.size() == maximumPath) {
+                int drop = maxPath.get(0) - maxPath.get(maxPath.size() - 1);
+                if (drop > maxDrop) {
+                    maxDrop = drop;
+                    index = ((Integer) entry.getKey()).intValue();
+                }
+            }
         }
+        System.out.println("Drop of calculated path: " + maxDrop);
+        System.out.println("Calculated path: " + path.get(index));
     }
 
-    public static void addAsArray(final String input, final int j, final boolean init) {
+    /**
+     * Adds all the values in an array
+     * @param input - Line from file
+     * @param j - row
+     * @param init - if the line is for the size of the array
+     */
+    private static void addAsArray(final String input, final int j, final boolean init) {
         final String[] s = input.split(" ");
         if (init) {
             a = new int[Integer.parseInt(s[0])][Integer.parseInt(s[1])];
@@ -56,13 +66,19 @@ public class Main {
     }
 
 
-    public static int longestPath(final int[][] array, final int i, final int j, int max) {
-        if (i==array.length)
-            return max;
-        if (j==array[0].length)
-            return longestPath(array, i+1, 0, max);
-        max = Math.max(getNextMax(array, i, j, array[i][j], true), max);
-        return longestPath(array, i, j+1, max);
+    private static int longestPath(final int[][] array, int max) {
+        for (int j = 0; j<array[0].length; j++) {
+            for (int i = 0; i<array.length; i++) {
+                max = Math.max(getNextMax(array, i, j, array[i][j], true), max);
+            }
+        }
+        return max;
+        // if (i==array.length)
+        //     return max;
+        // if (j==array[0].length)
+        //     return longestPath(array, i+1, 0, max);
+        // max = Math.max(getNextMax(array, i, j, array[i][j], true), max);
+        // return longestPath(array, i, j+1, max);
     }
 
     /**
@@ -74,16 +90,12 @@ public class Main {
      * @param first
      * @return
      */
-    public static int getNextMax(final int[][] array, final int i, final int j, int prevValue, final boolean first) {
+    private static int getNextMax(final int[][] array, final int i, final int j, int prevValue, final boolean first) {
 
         if (i<0 || j<0 || j==array.length || i == array[0].length)
             return 0;
 
-        if (first || (path2.size() != 0 && path2.get(path2.size()-1) < prevValue)) {
-            path2 = new ArrayList<>();
-            path.put(ctr++, path2);
-
-        }
+        putPath(first, prevValue);
 
         if (prevValue > array[i][j] || first) {
             prevValue = array[i][j];
@@ -108,7 +120,22 @@ public class Main {
      * @param west
      * @return
      */
-    public static int max(final int north, final int south, final int east, final int west) {
+    private static int max(final int north, final int south, final int east, final int west) {
         return Math.max(Math.max(north, south), Math.max(east, west));
+    }
+
+    private static void putPath(final boolean first, final int prevValue) {
+        if (first || (path2.size() != 0 && path2.get(path2.size()-1) < prevValue)) {
+            path2 = new ArrayList<>();
+
+            if (!first) {
+                path2 = new ArrayList<>(path.get(ctr-1));
+                while (path2.size() != 0 && prevValue > path2.get(path2.size() - 1)) {
+                    path2.remove(path2.size() - 1);
+                }
+            }
+            path.put(ctr++, path2);
+
+        }
     }
 }
