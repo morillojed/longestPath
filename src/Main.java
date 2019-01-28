d84575c1aed6325a8649cfcc361e3c16f6a6e308
jed.steven.e.morillo <jed.steven.e.morillo@accenture.com>
2019-01-28 15:31:41 +0800

    Partial Commit 1: gets the next max value, either north, south, east, west. Started with reading the file.

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,71 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
-        final Scanner in = new Scanner(System.in);
-        while (in.hasNextLine()) {
-            //read
+    int[] path;
+    int ctr=0;
+    int[][] a;
+
+    public static void main(String[] args) throws FileNotFoundException, IOException{
+        final File file = new File("/Users/jedstevenmorillo/Downloads/skirsesort.kitzbuehel/4x4.txt");
+        final BufferedReader br = new BufferedReader(new FileReader(file));
+        int j = 0;
+        while (br.readLine() != null) {
+            addAsArray(br.readLine(), j++);
         }
-        System.out.println("Hello World!");
     }
 
-    public int getNextMax(int [][] array, int i, int j, boolean first) {
-        if (array[i][j])
+    public static void addAsArray(final String input, final int j) {
+        final String[] s = input.split(" ");
+        for (int i=0; i<s.length; i++) {
+
+        }
+    }
+
+
+    public int longestPath(final int[][] array, final int i, final int j, final int max) {
+        return 1;
+    }
+
+    /**
+     * gets the next maximum value.
+     * @param array
+     * @param i
+     * @param j
+     * @param prevValue
+     * @param first
+     * @return
+     */
+    public int getNextMax(final int[][] array, final int i, final int j, final int prevValue, final boolean first) {
+
+        if (prevValue > array[i][j] || first) {
+            path[ctr++] = array[i][j];
+
+            int west = j != 0 ? getNextMax(array, i, j-1, array[i][j], false) : 0;
+            int east = j < array[0].length - 1 ? getNextMax(array, i, j+1, array[i][j], false) : 0;
+            int south = i < array.length - 1 ? getNextMax(array, i+1, j, array[i][j], false) : 0;
+            int north = i != 0 ? getNextMax(array, i-1, j, array[i][j], false) : 0;
+
+            return 1 + max(north, south, east, west);
+        }
+
+        return 0;
+    }
+
+    /**
+     * gets the highest number
+     * @param north
+     * @param south
+     * @param east
+     * @param west
+     * @return
+     */
+    public int max(final int north, final int south, final int east, final int west) {
+        return Math.max(Math.max(north, south), Math.max(east, west));
     }
 }
