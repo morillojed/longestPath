80a0f544501bdf9d351cf0ef23a520c95366e075
jed.steven.e.morillo <jed.steven.e.morillo@accenture.com>
2019-01-28 14:57:50 +0800

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        final Scanner in = new Scanner(System.in);
+        while (in.hasNextLine()) {
+            //read
+        }
+        System.out.println("Hello World!");
+    }
+
+    public int getNextMax(int [][] array, int i, int j, boolean first) {
+        if (array[i][j])
+    }
+}
